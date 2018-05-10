3eab3ab522c4f8c65594561cfa4f2bbb19721981
Push <36050583+PushyPants@users.noreply.github.com>
2018-05-10 04:57:51 -0500

    Started JS over from scratch - Got (word array/random word/key value arg/capitalization fix/used letter check/correct letter check/incorrect letter check/score adjust) working. Made adjustments to the html to make it look a bit better during testing and created JS to change elements along with the game play.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,78 @@
-// This creates the word bank from which words from the game will be chosen
-// I've created an array to do this
-var  wordBank = ["Old Fashioned" , "Tom Collins" , "Cubralibre"];
+//Create word bank array
+var wordBank = ["Old Fashioned", "Tom Collins", "Cubalibre", "Paloma"];
 
-// Math.random generates a number between 0 & 1.
-// We multiply that number by the amount of choices in the array using .length.
-// Math.Floor takes this number and rounds it down to the nearest whole number.
-var randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
-console.log(randomWord);
+var chances = 6;
 
-//While working on the functions, this is just here temporarily
-document.getElementById("word").innerHTML = document.getElementById("word").innerHTML + randomWord;
+//grab random word from array
+var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)].toLowerCase();
+    console.log(randomWord);
 
-/* We will need to create an inline <ul> with the letters of randomWord in the <li>s.
-I'm guessing that's a for() loop (or 2 with one nested) */
+// array/string to check agains alpha only characters
+var alphaOnly = "abcdefghijklmnopqrstuvwxyz";
+//empty array to add "used letters" to
+var usedLetterArr = [];
 
-
-// Set an EventListener to start the game with any key press
-// I'm adding this to the window because it's not dependent on a specific element
-// window.addEventListener("keypress", function () {
-//     // This will be the function that starts the game.
-//     console.log("This will be the function that starts the game.")
-
-// });
-
-// tester code to listen for keypress and output pressed key
-window.addEventListener("keypress", function(event) {
+//grabs the keyboard event
+document.onkeyup = function(event){
+    //stores the key "value" that was pressed
+    var keyPress = event.key;
     
-//pass value of key pressed to var key
-var keyPressed = event.key;
-
-//Pass the keyCode value so we know if the keys pressed were in the alpha range
-var keyCode = event.keyCode;
-
-//change value of "keyPressed" to uppercase for display purposes
-var upperKey = keyPressed.toLocaleUpperCase();
-
-//get current value of the letters id in the html
-var existingKey = document.getElementById("letters").innerHTML;
-
-    // Check to see if characters are letters by using keyCodes
-    if(keyCode >= 97 && keyCode <=122 ) {
-
-        //Now that we have a letter we need to check it against the Used Letter bank (array probably?)
-            //(if) used, send message to pick again
-            //(else) if not used, check it against the hangman word
-                //(if) correct, display letter
-                //(else) if wrong, subtract 1 from remaining choices
-        //Add letter to Used Letter Bank
-        
+    //checks to see if the key value is in the alpha string
+    if (alphaOnly.includes(keyPress)){
+        //changes the value to upper case
+        var upperKey = keyPress.toLocaleUpperCase();
+        //stores the inner html of the "usedLetters" div in the index.html
+        var usedLetters = document.getElementById("usedLetters").innerHTML;
 
         
+        //checks to see if the letter has bee tried yet
+        if(usedLetterArr.includes(keyPress)){
+           //flash css message that letter has been used.
+            document.getElementById("tempMessage").innerHTML = "That letter has already been used";
+            document.getElementById("tempMessage").className = "show";
+            setTimeout(function(){
+            document.getElementById("tempMessage").className = "hidden";
+            }, 1600);
+        } else {
+            //takes the valid keypress and adds it to the used array
+            usedLetterArr.push(keyPress);
+            //this is showing that it is updating the array
+            console.log(usedLetterArr);
+            //adds letter to the "usedLetters" div in index.html
+            document.getElementById("usedLetters").innerHTML = usedLetters + upperKey;  
+            
+            //checks to see if letter chosen is correct
+            if (randomWord.includes(keyPress)){
+                console.log("GOT ONE!");
+                //TO DO:  
+                    //find how many of that letter are in the word and where they are
+                    //uncover/display letter(s)
+            } else {
+                console.log("not one...");
+                document.getElementById("chances").innerHTML = "Remaining Chances: " + (chances - 1);
+                chances -= 1;
+            }
 
-
+            }
         
-        //get the element "letters" in the html and update/change its contents to the most recent content it had and add the latest key pressed.
-        document.getElementById("letters").innerHTML = existingKey + upperKey;
-        console.log("Alpha key pressed: " + keyPressed);
-
-
-
-
+        
+        
+       
+        
+    
+    
+    
+    
     } else {
-        // Display message - change class name to invoke css animation/change class back.
+        //flash css message that character is not valid.
         document.getElementById("tempMessage").innerHTML = "That is not a recognized character...";
         document.getElementById("tempMessage").className = "show";
             setTimeout(function(){
             document.getElementById("tempMessage").className = "hidden";
             }, 1600);
-    };
+    }
+}
 
-});
+
+
+
